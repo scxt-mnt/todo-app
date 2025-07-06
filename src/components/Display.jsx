30b346d2fd4e566ca3ce750ab178d4d5103c9819
Scott <scottboragay06@gmail.com>
2025-07-06 15:34:48 +0800

    updateL: Search compatability added

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,7 +2,8 @@
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { deleteTodos, updateTodos } from '../userSlice'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+
 
 const Display = () => {
   const selector = useSelector((state) => state.todo.value)
@@ -11,6 +12,8 @@ const Display = () => {
   const [id, setId] = useState(null);
   const [isTrue, setIsTrue] = useState(false)
   const [dropDown, setDropDown] = useState(false)
+  const [searchStore, setSearchStore] = useState('');
+  const findEngine = useRef({});
 
 
   const length = selector.length
@@ -22,11 +25,27 @@ const Display = () => {
   }
 
   const handleDropDown = () => {
-    setDropDown(!dropDown)
+    setDropDown(!dropDown);
   }
   const handleClose = () => {
-    setDropDown(false)
+    setDropDown(false);
   }
+  const handleSearch = (e) => {
+   e.preventDefault();
+   setTimeout(() => {
+    const look = selector.find((state) => state.todo.toLowerCase() === searchStore.toLowerCase());
+
+    if(look){
+      findEngine.current[look.id].scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      })
+    }
+   },100)
+  }
+
+  
+  
 
 
   return (
@@ -36,23 +55,27 @@ const Display = () => {
       <button className='bg-black text-white h-10 w-[100px] rounded-xl hover:bg-green-900 transition duration-1000' onClick={handleDropDown}>{`todo List ${dropDown ? ": " : ":"} ${length}`}</button>
       {dropDown === true && (
 
-        <main className='absolute overflow-y-auto grid bg-white border 
-      border-gray-400 shadow-2xl h-[38rem] w-[20rem] m-0 place-items-center gap-2 rounded-xl pb-10 '>
+
+        <main className='absolute overflow-y-auto grid bg-gray-100 border 
+      border-gray-300 shadow-2xl h-[38rem] w-[20rem] m-0 place-items-center gap-5 rounded-xl pb-11 '>
 
 
           <section className='w-full flex justify-end self-start sticky top-0 h-3 m-1 pr-2 z-10 text-3xl z-10 text-red-500'>
             <button onClick={handleClose}>X</button>
           </section>
 
-          <section className='self-start'>
-            {length === 0 ? <h1 className='text-black  text-xl'>no to-do listed</h1> : null}
+          <section className='self-start w-[300px] bg-black absolute top-2 '>
+            {length === 0 ? <h1 className=' absolute 
+            text-black  text-xl transform translate-y-[270px] translate-x-[78px] '>no to-do listed</h1> :<> 
+            <form onSubmit={handleSearch}> <input 
+              placeholder='search your todos here!' type='text' className='absolute w-[200px] rounded-lg text-center outline-none border focus:border-green-500 shadow-xl border-gray-300 focus:shadow-green-300 focus:shadow-xl' value={searchStore} onChange={(e) => setSearchStore(e.target.value)} /> <button className='bg-green-500 absolute right-[28px] rounded-xl text-sm w-[70px] h-[25px] top-[1px]'>search</button></form></>}
           </section>
 
 
           {selector.map((state) =>
 
 
-            <section className='grid relative  place-items-center min-w-[280px] max-w-[280px] min-h-[150px] h-[150px] bg-white text-black rounded-xl m-0 shadow-lg border border-gray-500  overflow-auto self-start p-0' key={state.id}>
+            <section className='grid relative  place-items-center min-w-[280px] max-w-[280px] min-h-[150px] h-[150px] bg-white text-black rounded-xl m-2 shadow-2xl border border-gray-200  overflow-auto self-start p-0' ref={(e) => findEngine.current[state.id] = e} >
 
               <section className='sticky flex justify-end top-0 font-bold text-lg self-start w-full h-1 pr-2'>
                 <button onClick={() => { dispatch(deleteTodos({ id: state.id })), setId(state.id), setId(null) }
@@ -70,9 +93,9 @@ const Display = () => {
               </section>
 
 
-              <p className='font-bold text-xl bg-green-400 rounded-md break-all mt-5 mr-3 ml-3 mb-5 p-2 self-start'>{state.todo ? state.todo : "no input"}</p>
+              {state.todo ? <p className='font-bold text-xl bg-green-400 rounded-md break-all mt-5 mr-3 ml-3 mb-5 p-2 self-start'>{state.todo}</p> : <p className='bg-red-500 rounded-md mt-5 mr-3 ml-3 mb-5 p-2 self-start'> no input </p>}
 
-              {id === state.id && (
+              { id === state.id && (
                 <>
                   {isTrue === true && (
                     <>
@@ -96,7 +119,7 @@ const Display = () => {
                   )}
 
                   <section className='absolute grid '>
-                    <textarea className='w-[13rem] h-[6rem]   whitespace-auto border border-red-300 p-2' value={update} onChange={(e) => setUpdate(e.target.value)} type="text" />
+                    <textarea className='w-[13rem] h-[6rem] border border-red-300 p-2 outline-none focus:border-red-900 z-20' value={update} onChange={(e) => setUpdate(e.target.value)} type="text" />
                     <button onClick={handleUpdate}>update</button>
                   </section>
                 </>

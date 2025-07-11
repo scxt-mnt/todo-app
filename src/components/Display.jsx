df22b5e2e428d1ba7683bc7828d6f5b48dbca22b
Scott <scottboragay06@gmail.com>
2025-07-12 07:38:31 +0800

    updates: updated user search assist

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { deleteTodos, updateTodos } from '../userSlice'
 import { useRef, useState } from 'react'
+import styles from './visibilty.module.css'
 
 
 const Display = () => {
@@ -13,7 +14,11 @@ const Display = () => {
   const [isTrue, setIsTrue] = useState(false)
   const [dropDown, setDropDown] = useState(false)
   const [searchStore, setSearchStore] = useState('');
-  const findEngine = useRef({});
+  const searchEngine = useRef({});
+  const noMatch = useRef();
+  const blanks = useRef();
+
+
 
 
   const length = selector.length
@@ -33,17 +38,27 @@ const Display = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     setTimeout(() => {
-      const look = selector.find((state) => state.todo.toLowerCase() === searchStore.toLowerCase());
-
-      if (look) {
-        findEngine.current[look.id].scrollIntoView({
-          behavior: 'smooth',
-          block: 'center'
-        })
+      const lookFor = selector.find(items => items.todo.toLowerCase().trim() === searchStore.toLowerCase().trim());
+      console.log(lookFor)
+      console.log(searchStore)
+      if (lookFor) {
+        noMatch.current.style.display = 'none',
+        blanks.current.style.display = 'none',
+          searchEngine.current[lookFor.id].scrollIntoView({
+            block: 'center',
+            behavior: 'smooth',
+          })
+        if (searchStore == "") {
+          blanks.current.style.display = 'block'
+        }
       }
+      else {
+        noMatch.current.style.display = 'block'
+      }
+      console.log(lookFor)
     }, 100)
+
   }
-    console.log('ci/cd testing');
 
 
 
@@ -57,25 +72,29 @@ const Display = () => {
 
 
         <main className='absolute overflow-y-auto grid bg-gray-100 border 
-      border-gray-300 shadow-2xl h-[38rem] w-[20rem] m-0 place-items-center gap-5 rounded-xl pb-11 '>
+      bordeqr-gray-300 shadow-2xl h-[38rem] w-[20rem] m-0 place-items-center gap-5 rounded-xl pb-11 '>
 
 
           <section className='w-full flex justify-end self-start sticky top-0 h-3 m-1 pr-2 z-10 text-3xl z-10 text-red-500'>
             <button onClick={handleClose}>X</button>
           </section>
 
-          <section className='self-start w-[300px] bg-black absolute top-2 '>
+          <section className='self-start w-[300px] bg-white absolute top-2 '>
             {length === 0 ? <h1 className=' absolute 
             text-black  text-xl transform translate-y-[270px] translate-x-[78px] '>no to-do listed</h1> : <>
               <form onSubmit={handleSearch}> <input
-                placeholder='search your todos here!' type='text' className='absolute w-[200px] rounded-lg text-center outline-none border focus:border-green-500 shadow-xl border-gray-300 focus:shadow-green-300 focus:shadow-xl' value={searchStore} onChange={(e) => setSearchStore(e.target.value)} /> <button className='bg-green-500 absolute right-[28px] rounded-xl text-sm w-[70px] h-[25px] top-[1px]'>search</button></form></>}
+                placeholder='search your todos here!' type='text' className='absolute w-[200px] rounded-lg text-center outline-none border focus:border-green-500 shadow-xl border-gray-300 focus:shadow-green-300 focus:shadow-xl' value={searchStore} onChange={(e) => setSearchStore(e.target.value)} /> <button className='bg-green-500 absolute right-[28px] rounded-xl text-sm w-[70px] h-[25px] top-[1px]'>search</button></form>
+              <p ref={noMatch} className={`${styles.noMatch} text-red-500 tranform translate-y-6 text-[13px]`}>no match found</p>
+              <p ref={blanks} className={`${styles.noMatch} text-red-500 tranform translate-y-6 text-[13px]`}>your search bar is blank</p>
+
+            </>}
           </section>
 
 
           {selector.map((state) =>
 
 
-            <section className='grid relative  place-items-center min-w-[280px] max-w-[280px] min-h-[150px] h-[150px] bg-white text-black rounded-xl m-2 shadow-2xl border border-gray-200  overflow-auto self-start p-0' ref={(e) => findEngine.current[state.id] = e} >
+            <section className='grid relative  place-items-center min-w-[280px] max-w-[280px] min-h-[150px] h-[150px] bg-white text-black rounded-xl m-2 shadow-2xl border border-gray-200  overflow-auto self-start p-0' ref={e => searchEngine.current[state.id] = e} >
 
               <section className='sticky flex justify-end top-0 font-bold text-lg self-start w-full h-1 pr-2'>
                 <button onClick={() => { dispatch(deleteTodos({ id: state.id })), setId(state.id), setId(null) }
